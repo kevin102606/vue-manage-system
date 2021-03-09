b4b0b4ded8e72e35d4c53cce898c7fe6990fbb44
Your Name <you@example.com>
2021-03-09 23:48:17 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,146 @@
+<template>
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+            <div>
+                <img src="../assets/heima.png" alt="" />
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主体区域 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside :width="isCollapse ? '64px' : '200px'">
+                <div class="toggle-button" @click="toggleCollapse">|||</div>
+                <!-- 侧边栏菜单区域 -->
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+                    <!-- 一级菜单 -->
+                    <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+                        <!-- 一级菜单模板区域 -->
+                        <template slot="title">
+                            <!-- 图标 -->
+                            <i :class="iconsObj[item.id]"></i>
+                            <!-- 文本 -->
+                            <span>{{ item.authName }}</span>
+                        </template>
+                        <!-- 二级菜单 -->
+                        <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+                            <template slot="title">
+                                <!-- 图标 -->
+                                <i class="el-icon-menu"></i>
+                                <!-- 文本 -->
+                                <span>{{ subItem.authName }}</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!-- 右侧内容主体 -->
+            <el-main>
+                <!-- 路由占位符 -->
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      activePath: ''
+    }
+  },
+  computed: {},
+  created () {
+    this.getMenuLIst()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  mounted () {},
+  watch: {},
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuLIst () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(this.menuList)
+    },
+    // 点击按钮，切换菜单折叠和展开
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+      console.log(this.activePath)
+    }
+  },
+  components: {}
+}
+</script>
+
+<style scoped lang="less">
+.home-container {
+    height: 100%;
+}
+
+.el-header {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    background-color: #373d41;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
+}
+
+.el-aside {
+    background-color: #333744;
+    .el-menu {
+        border-right: none;
+    }
+}
+
+.el-main {
+    background-color: #eaedf1;
+}
+
+.iconfont {
+    margin-right: 10px;
+}
+
+.toggle-button {
+    background-color: #4a5064;
+    color: #fff;
+    font-size: 10px;
+    line-height: 24px;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>Welcome</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  created () {},
+  mounted () {},
+  watch: {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style scoped lang="less"></style>
